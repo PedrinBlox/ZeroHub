6bf4f2d08c70ff638f65d65404b20569849a18ee
PedrinBlox <97545769+PedrinBlox@users.noreply.github.com>
2022-02-24 23:20:09 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PedrinBlox/ZeroHub/main/source.lua?token=GHSAT0AAAAAABR5K4RG6NW3VMUVR22S7KJIYQYD7HA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PedrinBlox/ZeroHub/main/source.lua"))()
